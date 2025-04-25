d4733430177bb7b05d05205409c98342a02a0003
Pablo Mariasch <pbmariasch@ad.untdf.edu.ar>
2025-04-25 15:03:33 -0300

    TP_03_E_07
    Se agrega el Ejercicio N.º 7 del Trabajo Práctico N.º 3

--- 03 Estructuras Condicionales/Mariasch_TP_03_Ejercicio_07.py
+++ 03 Estructuras Condicionales/Mariasch_TP_03_Ejercicio_07.py
@@ -1,16 +1,13 @@
 #Escribir un programa que solicite una frase o palabra al usuario. Si el string ingresado termina con vocal, añadir un signo de exclamación al final e imprimir el string resultante por pantalla; en caso contrario, dejar el string tal cual lo ingresó el usuario e imprimirlo por pantalla.
 
-# Solicitar frase o palabra al usuario
-texto = input("Ingrese una frase o palabra: ")
+# Pedimos al usuario que ingrese una frase o palabra
+frase = input("Por favor, ingrese una frase o palabra: ")
 
-# Verificar si el string termina con vocal
-if len(texto) > 0:
-    ultimo_caracter = texto[-1].lower()
-    if (ultimo_caracter == 'a' or ultimo_caracter == 'e' or 
-        ultimo_caracter == 'i' or ultimo_caracter == 'o' or 
-        ultimo_caracter == 'u' or ultimo_caracter == 'á' or
-        ultimo_caracter == 'é' or ultimo_caracter == 'í' or
-        ultimo_caracter == 'ó' or ultimo_caracter == 'ú'):
-        texto += "!"
-# Imprimir el resultado
-print(texto)
+# Para acceder a la última letra de un string podemos usar el índice -1
+# Si la última letra de la frase (frase[-1]) es una vocal, agregar un signo
+# de exclamación e imprimirla
+if frase[-1] in ("AEIOUaeiou"):
+  print(f"{frase}!")
+# Sino, imprimir la frase tal cual fue ingresada
+else:
+  print(frase)
