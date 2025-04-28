c3e8b73a2189413b711cae163179c7d3a8c672be
Pablo Mariasch <pbmariasch@ad.untdf.edu.ar>
2025-04-28 14:31:16 -0300

    TP_04_E_06
    Se agrega el Ejercicio N.º 6 del Trabajo Práctico N.º 4

---
+++ 04 Estructuras Repetitivas/Mariasch_TP_04_Ejercicio_06.py
@@ -0,0 +1,5 @@
+#Desarrolla un programa que imprima en pantalla todos los números pares comprendidos entre 0 y 100, en orden decreciente.
+
+# Usamos un bucle for para recorrer del 100 al 0, con paso de -2
+for i in range(100, -1, -2):
+    print(i) 
