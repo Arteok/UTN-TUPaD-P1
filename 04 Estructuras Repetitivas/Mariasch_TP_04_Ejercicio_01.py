1613a516834ff5a1135da8ca88dc487a01699f60
Pablo Mariasch <pbmariasch@ad.untdf.edu.ar>
2025-04-28 13:28:55 -0300

    TP_04_E_01
    Se agrega el Ejercicio N.º 1 del Trabajo Práctico N.º 4

---
+++ 04 Estructuras Repetitivas/Mariasch_TP_04_Ejercicio_01.py
@@ -0,0 +1,9 @@
+
+#Crea un programa que imprima en pantalla todos los números enteros desde 0 hasta 100 (incluyendo ambos extremos), en orden creciente, mostrando un número por línea.
+
+# Programa que imprime los números del 0 al 100, uno por línea
+
+# Usamos un bucle for para recorrer del 0 al 100
+for i in range(0, 101):
+    print(i)  # Mostramos cada número en pantalla
+
