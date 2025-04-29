b53f8750a24fa8837d2edbf7f77dc80c1b06a071
Pablo Mariasch <pbmariasch@ad.untdf.edu.ar>
2025-04-29 13:56:56 -0300

    TP_05_E_02
    Se agrega el Ejercicio N.º 2 del Trabajo Práctico N.º 5

---
+++ 05 Funciones/Mariasch_TP_05_Ejercicio_02.py
@@ -0,0 +1,10 @@
+#Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el indexing con números negativos!
+
+# Creamos una lista con cinco elementos que me gustan
+cosas_que_me_gustan = ["basket", "café", "videojuegos", "música", "programar"]
+
+# Accedemos al penúltimo elemento usando indexación negativa 
+penultimo = cosas_que_me_gustan[-2]
+
+# Mostramos el penúltimo elemento por pantalla
+print("El penúltimo elemento es:", penultimo)
