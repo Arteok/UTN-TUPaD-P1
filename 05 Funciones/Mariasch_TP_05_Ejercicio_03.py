8023805634f65f05070f531982245775b562dc59
Pablo Mariasch <pbmariasch@ad.untdf.edu.ar>
2025-04-29 14:06:58 -0300

    TP_05_E_03
    Se agrega el Ejercicio N.º 3 del Trabajo Práctico N.º 5

---
+++ 05 Funciones/Mariasch_TP_05_Ejercicio_03.py
@@ -0,0 +1,12 @@
+#Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por pantalla. Pista: para crear una lista vacía debes colocar los corchetes sin nada en su interior. Por ejemplo: lista_vacia = []
+
+# Creamos una lista vacía
+lista_vacia = []
+
+# Agregamos tres palabras usando append
+lista_vacia.append("Python")
+lista_vacia.append("C#")
+lista_vacia.append("JavaScript")
+
+# Imprimimos la lista resultante
+print("La lista resultante es:", lista_vacia)
