6a66cef6578d2bc374e692d1d4d4e59a0f7c0396
Pablo Mariasch <pbmariasch@ad.untdf.edu.ar>
2025-04-29 14:41:54 -0300

    TP_05_E_08
    Se agrega el Ejercicio N.º 8 del Trabajo Práctico N.º 5

---
+++ 05 Funciones/Mariasch_TP_05_Ejercicio_08.py
@@ -0,0 +1,12 @@
+#Crear una lista vacía llamada "dobles" y agregar el doble de 5, 10 y 15 usando append directamente. Imprimir la lista resultante por pantalla.
+
+# Creamos una lista vacía
+dobles = []
+
+# Agregamos el doble de 5, 10 y 15 usando append directamente
+dobles.append(5 * 2)
+dobles.append(10 * 2)
+dobles.append(15 * 2)
+
+# Imprimimos la lista resultante
+print("Lista de dobles:", dobles)
