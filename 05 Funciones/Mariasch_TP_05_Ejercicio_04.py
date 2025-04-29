394dd83465695c4bd1aba4027e8f9fdfd9990797
Pablo Mariasch <pbmariasch@ad.untdf.edu.ar>
2025-04-29 14:18:19 -0300

    TP_05_E_04
    Se agrega el Ejercicio N.º 4 del Trabajo Práctico N.º 5

---
+++ 05 Funciones/Mariasch_TP_05_Ejercicio_04.py
@@ -0,0 +1,13 @@
+#Reemplazar el segundo y último valor de la lista “animales” con las palabras “loro” y “oso”, respectivamente. Imprimir la lista resultante por pantalla. ¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el indexing con números negativos! animales = ["perro", "gato", "conejo", "pez"]
+
+# Lista original
+animales = ["perro", "gato", "conejo", "pez"]
+
+# Reemplazamos el segundo valor 
+animales[1] = "loro"
+
+# Reemplazamos el último valor 
+animales[-1] = "oso"
+
+# Imprimimos la lista resultante
+print("Lista actualizada:", animales)
