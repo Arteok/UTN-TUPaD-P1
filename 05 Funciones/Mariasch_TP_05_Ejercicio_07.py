0ba29cc350f07f60c7e93e8968dc8c15e5b3b81a
Pablo Mariasch <pbmariasch@ad.untdf.edu.ar>
2025-04-29 14:38:53 -0300

    TP_05_E_07
    Se agrega el Ejercicio N.º 7 del Trabajo Práctico N.º 5

---
+++ 05 Funciones/Mariasch_TP_05_Ejercicio_07.py
@@ -0,0 +1,11 @@
+#Reemplazar los dos valores centrales (índices 1 y 2) de la lista “autos” por dos nuevos valores cualesquiera. autos = ["sedan", "polo", "suran", "gol"]
+
+# Lista original
+autos = ["sedan", "polo", "suran", "gol"]
+
+# Reemplazamos los valores en los índices 1 y 2
+autos[1] = "camioneta"
+autos[2] = "coupe"
+
+# Mostramos la lista actualizada
+print("Lista actualizada:", autos)
