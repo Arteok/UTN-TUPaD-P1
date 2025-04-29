6dca71b9fda9c6819500940dcdd79ce2c44d25ed
Pablo Mariasch <pbmariasch@ad.untdf.edu.ar>
2025-04-29 14:35:03 -0300

    TP_05_E_06
    Se agrega el Ejercicio N.º 6 del Trabajo Práctico N.º 5

---
+++ 05 Funciones/Mariasch_TP_05_Ejercicio_06.py
@@ -0,0 +1,7 @@
+#Crear una lista con números del 10 al 30 (incluído), haciendo saltos de 5 en 5 y mostrar por pantalla los dos primeros.
+
+# Creamos la lista con números del 10 al 30, con saltos de 5
+numeros = list(range(10, 31, 5))
+
+# Mostramos los dos primeros elementos de la lista
+print("Los dos primeros números son:", numeros[0], "y", numeros[1])
