97c783610aae8a3f783d9b579965bbe7455071ff
Pablo Mariasch <pbmariasch@ad.untdf.edu.ar>
2025-04-29 14:29:59 -0300

    TP_05_E_05
    Se agrega el Ejercicio N.º 5 del Trabajo Práctico N.º 5

---
+++ 05 Funciones/Mariasch_TP_05_Ejercicio_05.py
@@ -0,0 +1,13 @@
+#Analizar el siguiente programa y explicar con tus palabras qué es lo que realiza.
+
+numeros = [8, 15, 3, 22, 7]
+numeros.remove(max(numeros))
+print(numeros)
+
+# 1 - Crea una lista llamada numeros con cinco valores: [8, 15, 3, 22, 7].
+
+# 2 - Encuentra el valor máximo de la lista usando max(numeros), que en este caso es 22.
+
+# 3 - Elimina ese valor máximo de la lista con numeros.remove(...), quitando el 22.
+
+# 4 - Imprime la lista final.
