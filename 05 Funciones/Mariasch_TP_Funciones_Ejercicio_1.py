9fe5972545f1afbb27221932ef438ac21b5aa0d2
Pablo Mariasch <pbmariasch@ad.untdf.edu.ar>
2025-05-28 13:13:13 -0300

    TP_Funciones_E_01
    Se agrega el Ejercicio N.º 1 del Trabajo Práctico de Funciones

---
+++ 05 Funciones/Mariasch_TP_Funciones_Ejercicio_1.py
@@ -0,0 +1,5 @@
+# Crear una función llamada imprimir_hola_mundo que imprima por pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el programa principal.
+
+# Llamada desde el programa principal
+import utils
+utils.imprimir_hola_mundo()
---
+++ 05 Funciones/utils.py
@@ -0,0 +1,3 @@
+# Definición de la función imprimir_hola_mundo
+def imprimir_hola_mundo():
+    print("Hola Mundo!")
