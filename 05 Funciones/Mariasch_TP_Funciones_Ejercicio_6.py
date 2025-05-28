400e98a0d0d39dc209a48e8d190f9e518bb6063d
Pablo Mariasch <pbmariasch@ad.untdf.edu.ar>
2025-05-28 15:49:35 -0300

    TP_Funciones_E_06
    Se agrega el Ejercicio N.º 6  del Trabajo Práctico de Funciones

---
+++ 05 Funciones/Mariasch_TP_Funciones_Ejercicio_6.py
@@ -0,0 +1,14 @@
+# Crear una función llamada tabla_multiplicar(numero) que reciba un número como parámetro y imprima la tabla de multiplicar de ese número del 1 al 10. Pedir al usuario el número y llamar a la función.
+
+def tabla_multiplicar(numero):
+    print(f"Tabla de multiplicar del {numero}:")
+    for i in range(1, 11):
+        resultado = numero * i
+        print(f"{numero} x {i} = {resultado}")
+
+# Solicitar el número al usuario
+
+numero = int(input("Ingresa un número para ver su tabla de multiplicar: "))
+tabla_multiplicar(numero)
+
+
